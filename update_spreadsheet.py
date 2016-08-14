deed2f1503dd0ed094ff55d96269bdf9a0ab7321
Craig Meinschein <cmeinsch@gmail.com>
2016-08-13 17:43:50 -0700

    Add skeleton for updating google sheet

---
+++ update_spreadsheet.py
@@ -0,0 +1,79 @@
+import argparse
+import gspread
+import json
+import sys
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+def get_sheet(credentials):
+    api = gspread.authorize(credentials)
+    return api.open('Abstractions.io Conference Schedule').get_worksheet(0)
+
+
+def read_credentials(file_name):
+    return ServiceAccountCredentials.from_json_keyfile_name(
+        file_name,
+        ['https://spreadsheets.google.com/feeds']
+    )
+
+
+def parse_args():
+    description = 'Update the Abstractions.io schedule Google Sheet.'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        'credentials',
+        help='Path to the JSON file containing the Google Sheets '
+             'service account credentials'
+    )
+    parser.add_argument('schedule', help='Path to the JSON file')
+    return parser.parse_args()
+
+
+def clear_sheet(sheet):
+    cell_list = [sheet.cell(row, column)
+                 for row in range(1, sheet.row_count + 1)
+                 for column in range(1, sheet.col_count + 1)]
+    for cell in cell_list:
+        cell.value = ''
+    sheet.update_cells(cell_list)
+
+
+def add_sheet_headers(sheet):
+    # sheet.
+    pass
+
+
+def update_sheet(sheet, schedule):
+    add_sheet_headers(sheet)
+
+
+def main():
+    args = parse_args()
+    with open(args.schedule) as f:
+        schedule = json.load(f)
+    credentials = read_credentials(args.credentials)
+    sheet = get_sheet(credentials)
+    clear_sheet(sheet)
+    update_sheet(sheet, schedule)
+
+    return 0
+
+
+if __name__ == "__main__":
+    status = main()
+    sys.exit(status)
+
+
+# A Start Date *
+# B End date *
+# C Headline *
+# D Text
+# E Media
+# F Media Credit
+# G Media Caption
+# H Media Thumbnail
+# I Type
+# J Tag (Room?) *
+# K Speaker *
+# L Speaker URL (image) *
+# M Description (of the talk) *
